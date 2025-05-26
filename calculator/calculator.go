c9fa2f8cb34a69182b28b61ee98fb535000d87df
IPampurin <I.Pampurin@yandex.ru>
2025-05-26 21:18:54 +0500

    calculator with map

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,5 +1,5 @@
 /*
-	Это просто примитивный калькулятор.
+	Это просто примитивный калькулятор. Однако, показалось интересной идея представления калькулятора в виде map, а не банальным switch-case.
 */
 
 package main
@@ -8,42 +8,54 @@ import (
 	"fmt"
 )
 
+var Calculator map[string](func(float64, float64) float64) = map[string](func(float64, float64) float64){
+
+	"+": func(x, y float64) float64 {
+		return x + y
+	},
+
+	"-": func(x, y float64) float64 {
+		return x - y
+	},
+
+	"*": func(x, y float64) float64 {
+		return x * y
+	},
+
+	"/": func(x, y float64) float64 {
+		if y == 0 {
+			fmt.Println("Делить на ноль нельзя!")
+			return 0
+		}
+		return x / y
+	},
+
+	"%": func(x, y float64) float64 {
+		if y == 0 {
+			fmt.Println("Делить на ноль нельзя!")
+			return 0
+		}
+		return float64(int(x) % int(y))
+	},
+}
+
 func main() {
 
-	var num1, num2, result float64
-	var sign, message string
+	var num1, num2 float64
+	var sign string
 
 	fmt.Print("Введите первое число (у дроби разделитель '.'): ")
 	fmt.Scan(&num1)
 
-	fmt.Print("Введите знак действия ('+', '-', '*', '/'): ")
+	fmt.Print("Введите знак действия ('+', '-', '*', '/', '%'): ")
 	fmt.Scan(&sign)
 
 	fmt.Print("Введите второе число (у дроби разделитель '.'): ")
 	fmt.Scan(&num2)
 
-	switch sign {
-	case "+":
-		result = num1 + num2
-	case "-":
-		result = num1 - num2
-	case "*":
-		result = num1 * num2
-	case "/":
-		if num2 == 0 {
-			message = "Делить на ноль нельзя!"
-			fmt.Println(message)
-			return
-		} else {
-			result = num1 / num2
-		}
-	case "%":
-		result = float64(int(num1) % int(num2))
-	default:
-		message = "неизвестная операция"
-		fmt.Println(message)
-		return
+	if _, ok := Calculator[sign]; ok {
+		fmt.Printf("Ответ: %v", Calculator[sign](num1, num2))
+	} else {
+		fmt.Println("неизвестная операция")
 	}
-
-	fmt.Printf("Ответ: %v", result)
 }
